1caf131bc232b5b5ca15cef18a399332b570a895
Avaneesh <avaneeshp92@gmail.com>
2019-02-24 14:56:40 +0530

    ok

---
+++ app.js
@@ -0,0 +1,10 @@
+const exp=require('express');
+const app=exp();
+const user=require('./Router/rout')
+const cors=require('cors');
+const port = process.env.PORT || '3000';
+ app.use(cors());
+ app.use("/",user);
+app.listen(port,()=>{
+    console.log("On port 3000");
+});
